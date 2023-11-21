e28cab517ebac063b7e7624aa6fc75a4f5e256a1
Max Loo <maxloopww@gmail.com>
2023-11-21 22:44:48 +0800

    first commit

---
+++ run.py
@@ -0,0 +1,23 @@
+# python3 run.py --model_path /home/maxloo/src/pastoring/llama-13b --adapter_path /home/maxloo/src/pastoring/adapter --weights_path /home/maxloo/src/pastoring/weights --use_tpu
+# Import libraries
+# from huggingface_hub import login
+# # Log in programmatically
+# login(token="")
+import torch
+import transformers
+import requests
+print(torch.cuda.is_available())
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Load model and adapter weights from local directory
+model = transformers.AutoModelForCausalLM.from_pretrained("/home/maxloo/src/pastoring/llama/llama-2-13b")
+model.to(device)
+adapter = transformers.AutoModelForCausalLM.from_pretrained("/home/maxloo/src/pastoring/adapter", config=transformers.configuration.AdapterConfig.from_json_file("adapter_config.json"))
+model.load_state_dict(adapter.state_dict())
+adapter.load_state_dict(model.state_dict())
+# Define prompt
+prompt = "Hello, I am a chatbot."
+# Perform inference
+response = model.generate(prompt, max_length=50)
+# Print response
+print(response)
+
